7ec33c008a42f9873d958dcbe12d2fb34bdcb2c7
Xie Yuheng <xyheme@gmail.com>
2023-02-24 10:50:54 +0800

    up

--- api/index.js
+++ api/index.js
@@ -2,7 +2,8 @@ import { createDatabase } from "fidb/lib/database"
 import { createRequestListener } from "fidb/lib/rest"
 import { resolve } from "node:path"
 
-const path = resolve(__filename, "../../databases/test")
+// const path = resolve(__filename, "../../databases/test")
+const path = resolve(process.cwd(), "databases/test")
 
 console.log({ path })
 
